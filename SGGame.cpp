c39c8188c24ad1d2a5a106a36751a4ce54cbbaab
JieunSummerPark <parkj2@student.swosu.edu>
2019-06-12 03:59:28 +0900

    초기 커밋.

---
+++ SGGame.cpp
@@ -0,0 +1,26 @@
+﻿#include "SpeedGuguGame.h"
+void main()
+{
+	srand((unsigned)time(NULL));
+
+	int nPlay;
+
+	printf("몇 번 게임을 하고 싶습니까? => ");
+	scanf_s("%d", &nPlay);
+
+	printf("\n\n[스피드구구단게임]\n\n");
+	printf(" 당신의 구구단 실력을 테스트하세요!!!\n");
+	printf(" %d번 테스트 하겠습니다.\n", nPlay);
+	printf(" 크게 심호흡 하시고...\n 준비되면 엔터를 누르세요...\n\n");
+	getchar();
+	//system("cls");	// 콘솔 화면을 깨끗이 지움
+
+	SpeedGuguGame game;
+	double score = game.play(nPlay);
+	printf("\n점수 = %4.1f점 (총 %4.1f초)\n", score, game.tElapsed);
+
+	/*
+	double score = playSpeedGugu(nPlay);
+	printf("\n점수 = %4.1f점 (총 %4.1f초)\n", score, tElapsed);
+	*/
+}
---
+++ SpeedGuguGame.h
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+extern double tElapsed;
+extern double playSpeedGugu(int nPlay);
+
+class SpeedGuguGame {
+	int NumGames = 0;
+	int NumWins = 0;
+	double Score = 0;
+
+	bool playGuguOnce() {
+		int a = rand() % 8 + 2;
+		int b = rand() % 8 + 2;
+		int result;
+
+		NumGames++;
+		printf("[문제%2d] %2d x %2d = ", NumGames, a, b);
+		scanf_s("%d", &result);
+		if (result == a * b) NumWins++;
+		return (result == a * b);
+	}
+
+public:
+	double tElapsed;
+	double play(int nPlay) {
+		clock_t t0 = clock();
+		for (int i = 0; i < nPlay; i++) {
+			if (playGuguOnce() == false)
+				printf("\t틀렸습니다.\n");
+		}
+		clock_t t1 = clock();
+		tElapsed = (double)(t1 - t0) / CLOCKS_PER_SEC;
+		Score = (NumGames > NumWins) ? 0.0
+			: 100 * (5.0 * NumGames - tElapsed) / (5.0 * NumGames);
+		return Score;
+	}
+};
